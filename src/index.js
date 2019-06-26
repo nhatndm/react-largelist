05a37c887114457113694b03acaf41e4d62587ce
nhatndm <nhatndm1193@gmail.com>
2019-06-26 14:37:07 +0700

    change name in package.json and title in example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
+ReactDOM.render(
+  <div>A new large list handling library for react</div>,
+  document.getElementById("root")
+);
